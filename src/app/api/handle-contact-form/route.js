60319456c0876704287d365a81f00f96bc8746bd
LetSamEqual <samahenderson24@gmail.com>
2024-06-20 15:40:42 +1000

    integrated lambda

--- src/app/api/handle-contact-form/route.js
+++ src/app/api/handle-contact-form/route.js
@@ -1,4 +1,14 @@
 import { NextResponse } from "next/server";
+const aws = require("aws-sdk");
+
+// const { Parameters } = await (new aws.SSM())
+//   .getParameters({
+//     Names: ["SENDGRID_API_KEY"].map(secretName => process.env[secretName]),
+//     WithDecryption: true,
+//   })
+//   .promise();
+
+// Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
 
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
